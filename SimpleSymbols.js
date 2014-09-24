d8be3cb50a75fc1608c84c2db76cb62705d117ba
Aaron Rumery <bloodhawk@users.noreply.github.com>
2014-09-24 09:03:07 -0600

    finish simplesymbols

---
+++ SimpleSymbols.js
@@ -0,0 +1,17 @@
+function SimpleSymbols(str) { 
+  var alphaAry = "abcdefghijklmnopqrstuvwxyz".split("");
+  var result;
+  for (var i = 0; i < str.length; i++) {
+    for(var x = 0; x < alphaAry.length; x++) {
+      if(str[i] === alphaAry[x]){
+        if(str[i-1] === "+" && str[i+1] === "+") {
+           result = true;
+        }
+        else {
+           return false;
+        }  
+      }  
+    }  
+  }  
+  // code goes here  
+  return result; 
